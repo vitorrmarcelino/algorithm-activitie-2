0f080e65726db19a7f0e2d8bee384635123bddc9
vitorrmarcelino <vitorrmarcelino09@gmail.com>
2025-05-21 19:52:06 -0300

    chore: remove choice and calculating both

--- main.c
+++ main.c
@@ -4,18 +4,9 @@
 #include "utils/mdcCalculator.c"
 
 void main(){
-    int choice, n1, n2, result, stop;
+    int n1, n2, mmc, mdc, stop;
 
     do{
-        printf("MMC[1]  MDC[2] \n");
-        scanf("%d", &choice);
-
-        while (choice != 1 && choice != 2) {
-            printf("Invalid choice. Please enter 1 for MMC or 2 for MDC.\n");
-            printf("MMC[1]  MDC[2] \n");
-            scanf("%d", &choice);
-        }
-
         printf("Enter the first number: \n");
         scanf("%d", &n1);
 
@@ -30,16 +21,11 @@ void main(){
             scanf("%d", &n2);
         }
 
-        switch (choice) {
-            case 1:
-                result = mmcCalculator(n1, n2);
-                printf("MMC: %d\n", result);
-                break;
-            case 2:
-                result = mdcCalculator(n1, n2);
-                printf("MDC: %d\n", result);
-                break;
-        }
+        mmc = mmcCalculator(n1, n2);
+        mdc = mdcCalculator(n1, n2);
+
+        printf("MMC: %d\n", mmc);
+        printf("MDC: %d\n", mdc);
 
         printf("Do you want to stop? YES[1] NO[2]\n");
         scanf("%d", &stop);
